c78f17911dc6c20e375f9b15b823a35ed0570123
David Ovčačík <david.ovcacik@gmail.com>
2023-11-06 21:58:03 +0100

    Adding points

--- nuxt-app/pages/cad/index.vue
+++ nuxt-app/pages/cad/index.vue
@@ -6,18 +6,22 @@
 
 <script setup lang="ts">
 	import {
-		AmbientLight,
-		DirectionalLight,
+		AmbientLight, BufferGeometry,
+		DirectionalLight, Float32BufferAttribute,
 		Mesh,
 		MeshPhongMaterial,
-		PerspectiveCamera,
+		PerspectiveCamera, Plane, Points, PointsMaterial, Raycaster,
 		Scene,
-		SphereGeometry, TextureLoader,
+		SphereGeometry, TextureLoader, Vector2, Vector3,
 		WebGLRenderer
 	} from 'three';
 	import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+	import {useMouse} from '@vueuse/core';
+	import {lineSphereIntersection} from '~/src/intersections';
 
 	const canvas = ref<HTMLCanvasElement | null>(null);
+
+	const mouse = useMouse({target: canvas});
 
 	onMounted(() => {
 		if (!canvas.value) return;
@@ -28,11 +32,18 @@
 
 		camera.position.z = 5;
 
+		const clippingPlane = new Plane(new Vector3(0, 0, 1));
+
 		const testureLoader = new TextureLoader();
 		const earthTexture = testureLoader.load('/assets/cad/textures/2k_earth_daymap.jpg');
 
 		const sphereGeometry = new SphereGeometry(1, 32, 32);
-		const sphereMaterial = new MeshPhongMaterial({specular: 0x555555, shininess: 30, map: earthTexture});
+		const sphereMaterial = new MeshPhongMaterial({
+			specular: 0x555555,
+			shininess: 30,
+			map: earthTexture,
+			depthTest: false,
+		});
 		const ambientLight = new AmbientLight(0x404040);
 
 		const earth = new Mesh(sphereGeometry, sphereMaterial);
@@ -48,8 +59,58 @@
 
 		renderer.setSize(window.innerWidth, window.innerHeight);
 
+		const pointer = new Vector2();
+
+		watch([mouse.x, mouse.y], () => {
+			pointer.x = (mouse.x.value / window.innerWidth) * 2 - 1;
+			pointer.y = -(mouse.y.value / window.innerHeight) * 2 + 1;
+		});
+
+		renderer.localClippingEnabled = true;
+
+		const pointsMaterial = new PointsMaterial({
+			color: 0xff0000,
+			size: 5,
+			clippingPlanes: [clippingPlane],
+			sizeAttenuation: false,
+		});
+
+
+		let mouseDownTimestamp = 0;
+
+		canvas.value.addEventListener('mousedown', () => {
+			mouseDownTimestamp = Date.now();
+		});
+		canvas.value.addEventListener('click', () => {
+			if (mouseDownTimestamp + 200 < Date.now()) return;
+
+			const raycaster = new Raycaster();
+			raycaster.setFromCamera(pointer, camera);
+
+			const [intersection] = lineSphereIntersection(
+				raycaster.ray.origin,
+				raycaster.ray.origin.clone().add(raycaster.ray.direction),
+				earth.position,
+				earth.geometry.parameters.radius,
+			);
+
+			if (!intersection) return;
+
+			const vertices = [intersection.x, intersection.y, intersection.z];
+			const geometry = new BufferGeometry();
+			geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
+			const points = new Points(geometry, pointsMaterial);
+			scene.add(points);
+		});
+
 		function render() {
 			orbitControls.update();
+
+			const cameraDirection = new Vector3(0);
+			camera.getWorldDirection(cameraDirection);
+			clippingPlane.normal.copy(cameraDirection).negate();
+			clippingPlane.constant = -1 + Math.atan(camera.position.length() - 1) / Math.PI * 2;
+
 			renderer.render(scene, camera);
 			requestAnimationFrame(render);
 		}
---
+++ nuxt-app/src/intersections.ts
@@ -0,0 +1,50 @@
+import {Vector3} from 'three';
+
+export function lineSphereIntersection(
+	linePoint0: Vector3,
+	linePoint1: Vector3,
+	circleCenter: Vector3,
+	circleRadius: number,
+): Vector3[] {
+	// http://www.codeproject.com/Articles/19799/Simple-Ray-Tracing-in-C-Part-II-Triangles-Intersec
+
+	const {x: cx, y: cy, z: cz} = circleCenter;
+	const {x: px, y: py, z: pz} = linePoint0;
+
+	const vx = linePoint1.x - px;
+	const vy = linePoint1.y - py;
+	const vz = linePoint1.z - pz;
+
+	const A = vx * vx + vy * vy + vz * vz;
+	const B = 2.0 * (px * vx + py * vy + pz * vz - vx * cx - vy * cy - vz * cz);
+	const C = px * px - 2 * px * cx + cx * cx + py * py - 2 * py * cy + cy * cy +
+		pz * pz - 2 * pz * cz + cz * cz - circleRadius * circleRadius;
+
+	// discriminant
+	const D = B * B - 4 * A * C;
+
+	if (D < 0) return [];
+
+	const t1 = (-B - Math.sqrt(D)) / (2.0 * A);
+
+	const solution1 = new Vector3(
+		linePoint0.x * (1 - t1) + t1 * linePoint1.x,
+		linePoint0.y * (1 - t1) + t1 * linePoint1.y,
+		linePoint0.z * (1 - t1) + t1 * linePoint1.z,
+	);
+
+	if (D == 0) return [solution1];
+
+	const t2 = (-B + Math.sqrt(D)) / (2.0 * A);
+	const solution2 = new Vector3(
+		linePoint0.x * (1 - t2) + t2 * linePoint1.x,
+		linePoint0.y * (1 - t2) + t2 * linePoint1.y,
+		linePoint0.z * (1 - t2) + t2 * linePoint1.z,
+	);
+
+	// prefer a solution that's on the line segment itself
+
+	if (Math.abs(t1 - 0.5) < Math.abs(t2 - 0.5)) return [solution1, solution2];
+
+	return [solution2, solution1];
+}
